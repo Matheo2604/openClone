b271d11395aea70b33ffe70552cc77b44700a601
Matheo2604 <libeau.matheo@gmail.com>
2024-03-19 16:06:51 +0100

    u

--- test.sh
+++ test.sh
@@ -12,16 +12,8 @@ nombre_interfaces=$(($(ip -o link show | wc -l) - 1))
 if [ $num_interfaces -gt 1 ]; then
     echo "Il y a $nombre_interfaces interfaces réseau disponibles."
     while true; do
-        read -p "Voulez-vous utiliser nftables (N), de l'agrégation (A), ou rien (R) ? [N/A/R]: " choice
+        read -e -p "Vous pouvez mettre en place ces fonctionnalité:\n1 - L'agrégation de liens\n2 - Nftables\n3 - Ne rien mettre en place\n" choice
         case $choice in
-            [Nn]* )
-                echo "Vous avez choisi d'utiliser nftables."
-                afficher_interfaces
-                read -p "Quelle interface voulez-vous utiliser pour le LAN ? " lan_interface
-                read -p "Quelle interface voulez-vous utiliser pour le WAN ? " wan_interface
-                echo "Interfaces choisies pour le LAN : $lan_interface, pour le WAN : $wan_interface"
-                # Ajoutez ici les commandes pour configurer nftables avec les interfaces choisies
-                ;;
             [Aa]* )
                 echo "Vous avez choisi d'utiliser de l'agrégation."
                 afficher_interfaces
@@ -29,6 +21,14 @@ if [ $num_interfaces -gt 1 ]; then
                 read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
                 echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
                 # Ajoutez ici les commandes pour configurer l'agrégation avec les interfaces choisies
+                ;;
+            [Nn]* )
+                echo "Vous avez choisi d'utiliser nftables."
+                afficher_interfaces
+                read -p "Quelle interface voulez-vous utiliser pour le LAN ? " lan_interface
+                read -p "Quelle interface voulez-vous utiliser pour le WAN ? " wan_interface
+                echo "Interfaces choisies pour le LAN : $lan_interface, pour le WAN : $wan_interface"
+                # Ajoutez ici les commandes pour configurer nftables avec les interfaces choisies
                 ;;
             * )
                 echo "Vous avez choisi de ne rien faire."
