2e7f849bd9305173d59676d27f02e3fbd1ed6daf
Matheo2604 <libeau.matheo@gmail.com>
2024-03-25 20:37:06 +0100

    u

--- test.sh
+++ test.sh
@@ -12,40 +12,27 @@ nombre_interfaces=$(($(ip -o link show | wc -l) - 1))
 if [ $nombre_interfaces -gt 1 ]; then
     echo "Il y a $nombre_interfaces interfaces réseau disponibles."
 
-    while true; do
-        echo -e "Vous pouvez mettre en place l'une de ces fonctionnalités :\n1 - L'agrégation de liens\n2 - Nftables\n3 - Ne rien mettre en place"
-        read -p "Votre choix : " choice
+    read -p "Voulez-vous mettre en place de l'agrégation de liens ? [y|n] " choice_aggregation
+    if [ "$choice_aggregation" == "y" ]; then
+        echo "Vous avez choisi d'utiliser de l'agrégation."
+        Afficher_interfaces
+        read -p "Entrez le nom de la première interface pour l'agrégation : " interface1
+        read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
+        echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
+        # Ajouter ici des commandes pour configurer l'agrégation avec les interfaces choisies
+    fi
 
-        case $choice in
-            1* )
-                echo "Vous avez choisi d'utiliser de l'agrégation."
-                Afficher_interfaces
-                read -p "Entrez le nom de la première interface pour l'agrégation : " interface1
-                read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
-                echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
-                # Ajouter ici des commandes pour configurer l'agrégation avec les interfaces choisies
-                ;;
-            2* )
-                echo "Vous avez choisi d'utiliser nftables."
-                Afficher_interfaces
-                read -p "Quelle interface sera utiliser comme LAN ? " lan_interface
-                read -p "Quelle interface sera utiliser comme WAN ? " wan_interface
-                echo "Interfaces choisies pour le LAN : $lan_interface, pour le WAN : $wan_interface"
-                # Ajouter ici des commandes pour configurer nftables avec les interfaces choisies
-                break
-                ;;
-            3* )
-                echo "Vous avez choisi de ne rien faire."
-                #fonction pour choisir l'interface reseau
-                break
-                ;;
-            * )
-                echo "Choix invalide. Veuillez choisir une option valide."
-                ;;
-        esac
-    done
+    read -p "Voulez-vous mettre en place le système nftables ? [y|n] " choice_nftables
+    if [ "$choice_nftables" == "y" ]; then
+        echo "Vous avez choisi d'utiliser nftables."
+        Afficher_interfaces
+        read -p "Quelle interface sera utilisée comme LAN ? " lan_interface
+        read -p "Quelle interface sera utilisée comme WAN ? " wan_interface
+        echo "Interfaces choisies pour le LAN : $lan_interface, pour le WAN : $wan_interface"
+        # Ajouter ici des commandes pour configurer nftables avec les interfaces choisies
+    fi
+
 else
-    # mettre ceci dans une fonction 2
     echo "Il n'y a qu'une interface réseau disponible."
     read -p "Quelle interface souhaitez-vous utiliser ? " interface
 fi
