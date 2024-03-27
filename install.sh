36c12ee7e975c5e55707dca5f1beca22102f3289
Matheo2604 <libeau.matheo@gmail.com>
2024-03-27 20:41:00 +0100

    i

--- install.sh
+++ install.sh
@@ -44,13 +44,14 @@ if [ $nombre_interfaces -gt 1 ]; then
         
         aggregation=true
         sudo apt -y install ifenslave
+        echo ""
         Afficher_interfaces
         echo ""
 
         read -p "Entrez le nom de la première interface pour l'agrégation : " interface1
         read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
         echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
-        echo -e "une nouvelle interface nommer bond0 vient d'etre creer\n" 
+        echo -e "\nune nouvelle interface nommer bond0 vient d'etre creer\n" 
         # Ajouter ici des commandes pour configurer l'agrégation avec les interfaces choisies
 
     fi
