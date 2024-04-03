46d3081d5933468d500b809ee54b0a0c3635368f
Matheo2604 <libeau.matheo@gmail.com>
2024-04-03 22:10:42 +0200

    l

--- ressource/scripts/test.sh
+++ ressource/scripts/test.sh
@@ -10,14 +10,14 @@ fi
 nombre_partitions=$1
 
 # Appel du script externe pour obtenir le nom_disque et taille_une_partition
-output=$(./partitionnage.sh)
+output=$(./partitionnage.sh $nombre_partitions)
 read nom_disque taille_une_partition <<< "$output"
 
 # Suppression de toutes les partitions et tables de partition sur nom_disque
-sudo /sbin/sfdisk --delete $nom_disque
+sudo /usr/sbin/sfdisk --delete $nom_disque
 
 # Création d'une nouvelle table de partition
-sudo /sbin/sfdisk $nom_disque << EOF
+sudo /usr/sbin/sfdisk $nom_disque << EOF
 label: dos
 unit: sectors
 
@@ -25,12 +25,9 @@ unit: sectors
 ,4096000,83
 EOF
 
-# Calcul de la taille de partition restante
-taille_restante=$((taille_une_partition - 4096000 * 2))
-
 # Création des partitions supplémentaires
 for ((i=1; i<=$nombre_partitions; i++)); do
-    sudo /sbin/sfdisk $nom_disque << EOF
-,${taille_restante}s,83
+    sudo /usr/sbin/sfdisk $nom_disque << EOF
+,${taille_une_partition}s,83
 EOF
 done
