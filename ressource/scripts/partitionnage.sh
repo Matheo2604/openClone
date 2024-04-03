1c8d94b29978757367b07129168b6dd94e64ac43
Matheo2604 <libeau.matheo@gmail.com>
2024-04-03 21:48:50 +0200

    f

--- ressource/scripts/partitionnage.sh
+++ ressource/scripts/partitionnage.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-
-
-# Vérifie si les paramètres ont été fournis en ligne de commande
-
-if [ $# -eq 2 ]; then
-
-
- disque_cible="$1"
-
-
+# Recupere le nombre de partiotion voulu par paramètre
+if [ $# -ne 1 ]; then
+ echo "Usage: $0 <nombre_partition>"
+    exit 1
 fi
 
+nombre_partition=$1
 
-# Vérifie si /sys/firmware/efi existe
+# Recherche du disque le plus grand disponible sur le poste
+disque=$(lsblk -b -d | grep disk | sort -k 4 -nr | head -n 1)
 
-[[ -d "/sys/firmware/efi" ]] && uefi=1 || uefi=0
+# Extraire le nom et la taille du disque le plus grand
+disque_nom=$(echo $disque | awk '{print $1}')
+disque_taille=$(echo $disque | awk '{print $4}')
 
-echo $disque_cible
+# Calculer la taille du disque en fonction du diviseur
+disque_partitionner=$((disque_taille / nombre_partition))
+
+# Soustraire 4096000 de la taille du disque l'equivalent de 2Gio (1Mio = 2048 secteur)
+disque_taille_finale=$((disque_partitionner - 4096000))
+
+if [ $disque_taille_finale -lt 4096000 ]; then
+    echo "Erreur : La taille finale du disque est inférieure à 4096000."
+    exit 1
+fi
+
+echo "$disque_nom $disque_taille_finale"
