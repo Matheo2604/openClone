bc602b54cf32e7279a9013c4e5db4dd5520221c8
Matheo2604 <libeau.matheo@gmail.com>
2024-04-03 22:33:24 +0200

    qs

--- ressource/scripts/test1.sh
+++ ressource/scripts/test1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Vérification du nombre de paramètres
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 nombre_partitions"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 nombre_partitions fichier_sortie"
     exit 1
 fi
 
-# Récupération du nombre de partitions
+# Récupération du nombre de partitions et du nom du fichier de sortie
 nombre_partitions=$1
+fichier_sortie=$2
 
 # Appel du script externe pour obtenir le nom_disque et taille_une_partition
 output=$(./partitionnage.sh $nombre_partitions)
@@ -17,25 +18,28 @@ read nom_disque taille_une_partition <<< "$output"
 echo "Nom du disque: $nom_disque"
 echo "Taille d'une partition: $taille_une_partition"
 
-# Suppression de toutes les partitions et tables de partition sur nom_disque
-sudo /usr/sbin/sfdisk --delete /dev/$nom_disque
+# Redirection de la sortie vers le fichier de sortie
+{
+    # Suppression de toutes les partitions et tables de partition sur nom_disque
+    sudo /usr/sbin/sfdisk --delete /dev/$nom_disque
 
-# Création d'une nouvelle table de partition
-sudo /usr/sbin/sfdisk /dev/$nom_disque << EOF
+    # Création d'une nouvelle table de partition
+    sudo /usr/sbin/sfdisk /dev/$nom_disque << EOF
 label: dos
 unit: sectors
 
-,4096000,0c,*
-,4096000,83
+,4096000,boot,*
+,4096000,grub
 EOF
 
-# Affichage de quelques informations pour le débogage
-echo "Création des partitions supplémentaires..."
+    # Affichage de quelques informations pour le débogage
+    echo "Création des partitions supplémentaires..."
 
-# Création des partitions supplémentaires
-for ((i=3; i<=$nombre_partitions+2; i++)); do
-    sudo /usr/sbin/sfdisk /dev/$nom_disque << EOF
+    # Création des partitions supplémentaires
+    for ((i=3; i<=$nombre_partitions+2; i++)); do
+        sudo /usr/sbin/sfdisk /dev/$nom_disque << EOF
 ,${taille_une_partition}s,83
 EOF
-    echo "Partition $i créée."
-done
+        echo "Partition $i créée."
+    done
+} > "$fichier_sortie" 2>&1   # Redirection de la sortie standard (1) et de la sortie d'erreur (2) vers le fichier de sortie
