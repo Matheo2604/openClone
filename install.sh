4c4c60ff592210ba8573248b9d1f1a85745aa01d
Matheo2604 <libeau.matheo@gmail.com>
2024-05-27 16:44:55 +0200

    v

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Mettre tous les services dans des scripts a part
-# Ajouter un fichier de config
-# Faire script principal partie Elouen
-# KEA
-# Nom du site a changer
-# CONFIG RESEAU
-# MariaDB creer un compte avec un mot de passe randomiser par default
-# Fichier de conf rw r r
-# dossier rwxr-xr-x
-# lancement avec bash
-# reset pour clear le terminal
+# TO DO
+# Put every services in a different script
+# Make the file.ini useful
+# Implementation of KEA
+# Change the name of web site and lets the user chose it
+# in the readME need to presice to start the script with the commande bash
+# In the end restart every services then do a final check of these services
+# MariaDB create an account with random password by default
+# Doesn't need to start the script without having root permission by consequences ask the user to create an account 
+# Ask to change the password with an script when the first remote connexion is done DeBootStrap 
+
+# THING TO DO BUT NO PRIORITIES 
+# CONFIGURE THE NETWORK INTERFACE WITH ANOTHER WAY  
+# Do the most importante script (the part of Elouen)
+# Add the possibility to have an ssl certificat with lets encrypt for an web server
+# MariaDB remote connexion
+
+# THING THAT CAN HELP FOR THE FUTUR
+# reset to clear the terminal
 # source <(grep = config.ini)
-# default user pas besoin de sudo et par consequen debut demander s'y etre sure de lancer en root 
-# dire demander s'y il veut creer un user ou non
-# verifier s'y besoin user autrement possibiliter de le supprimer a la fin ???
-# demander de changer de mot de passe pour le compte debootstrap l'hors de la premières connexion
-# dans le futur peut être ajouter possibilité d'ajouter un certificat ssl avec lets encrypt penser a changer le .ini et faire un script tiers
-# MariaDB REMOTE CONNEXION
 
 if [ "$(id -u)" -eq 0 ]; then
  echo "Ce script ne doit pas être exécuté en tant que root."
@@ -26,7 +28,7 @@ fi
 cd "$(dirname $0)"  
 username="$(whoami)"
 
-# Fonction pour afficher les interfaces disponibles
+# Fonction to show network interface
 Afficher_interfaces() {
 
     echo "Interfaces disponibles :"
@@ -44,10 +46,10 @@ read -p "Quelle est l'IP du sous résaux LAN (exemple: 192.168.1.0):" IP_LAN_SR
 
 }
 
-# Trouver le nombre d'interfaces réseau
+# Find the number of network interface
 nombre_interfaces=$(($(ip -o link show | wc -l) - 1))
 
-# Connaitre si l'utilisateur choisit l'une de c'est fonctionnalite
+# Know if the user chose one of these fonctionality
 aggregation=false
 nftables=false
 
@@ -68,7 +70,7 @@ if [ $nombre_interfaces -gt 1 ]; then
         read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
         echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
         echo -e "\nune nouvelle interface nommer bond0 vient d'etre creer\n" 
-        # Ajouter ici des commandes pour configurer l'agrégation avec les interfaces choisies
+        # add here commandes to configure l'agréggation with the chosen interfaces
 
     fi
 
@@ -88,7 +90,7 @@ if [ $nombre_interfaces -gt 1 ]; then
         read -p "Quelle est l'IP du sous résaux LAN (exemple: 192.168.1.0):" IP_NAT_SR
         read -p "Quelle est l'IP du routeur du réseaux NAT (exemple: 192.168.1.254):" Routeur
 
-        # Configuration Nftables
+        # Configure Nftables
         sudo sed -i \
           -e "s/{Interface_NAT}/$Interface_NAT/g" \
           -e "s/{IP_NAT_SR}/$IP_NAT_SR/g" \
@@ -191,7 +193,7 @@ sudo service networking restart
 sudo ip r add default via $Routeur
 
 
-# Mise a jour et installation des paquets
+# Update & install of paquets needed
 
 sudo apt update && sudo apt -y upgrade
 sudo apt -y install apache2 atftpd nfs-kernel-server debootstrap php bind9 isc-dhcp-server wget mariadb-server
@@ -199,10 +201,10 @@ sudo apt -y install apache2 atftpd nfs-kernel-server debootstrap php bind9 isc-d
 
 #!!!!!!!!!!!wget https://cdimage.kali.org/kali-2023.4/kali-linux-2023.4-live-amd64.iso
 
-#alternative du dhcp et dns non obsolete avec "kea"
+# alternative for dhcp & dns non-deprecated with "kea"
 
 
-# Configuration du serveur TFTP
+# Configure TFTP server
 
 sudo sudo mkdir /srv/tftp
 sudo mv ressource/serveur_transfert/atftpd /etc/default/atftpd
@@ -210,7 +212,7 @@ sudo systemctl restart atftpd.service
 sudo chmod -R ugo+rw /srv/tftp/
 
 
-# Ajouts des fichiers de boot linux (vmlinuz & initrd & grub.cfg)
+# Add Boot file for linux (vmlinuz & initrd & grub.cfg)
 
 sudo grub-mknetdir
 sudo sed -i "s/{IP_LAN}/$IP_LAN/g" ressource/grub.cfg
@@ -218,7 +220,7 @@ sudo mv ressource/grub.cfg /srv/tftp/boot/grub/grub.cfg
 
 
 
-#Configuration du serveur NFS
+#Configure NFS server
 
 
 sudo mkdir /srv/nfs
@@ -233,7 +235,7 @@ sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
 
 
-# Configuration du Debootstrap
+# Configure Debootstrap
 
 
 sudo sudo mkdir /srv/nfs/debian
@@ -257,7 +259,7 @@ sudo mkdir /srv/nfs/debian/etc/systemd/system/getty@tty1.service.d/
 sudo sudo mv ressource/linux_maintenance/override.conf /srv/nfs/debian/etc/systemd/system/getty@tty1.service.d/override.conf
 
 
-# Configuration du serveur WEB / HTTP
+# Configure WEB server / HTTP
 
 sudo mv ressource/www /srv/www
 sudo mv ressource/serveur_transfert/site.conf /etc/apache2/sites-available/
@@ -267,9 +269,9 @@ sudo chown www-data /srv/www/ -Rf
 sudo systemctl restart apache2.service
 
 
-# Configuration MariaDB
-
-#Securiser bdd 
+# Configure MariaDB
+# Secured database 
+# Remote connexion
 #yes | sudo mysql_secure_installation 
 
 #PB
@@ -286,6 +288,7 @@ sudo mysql  << EOL
   Hostname VARCHAR(30));
 
 EOL
+
 #mysql --password=1234 --user=root --host=localhost << eof
 #create database ownclouddb;
 #grant all privileges on ownclouddb.* to root@localhost identified by "1234";
@@ -303,7 +306,7 @@ EOL
 
 
 # Configuration du DHCP
-#!!!! si pas nftables besoins routeur
+#!!!! if no nftables routeur needed
 
 IP_LAN_TABLEAU=( $(echo $IP_LAN | tr "." " ") )
 sudo sed -i \
@@ -336,9 +339,5 @@ sudo sed -i \
   ressource/dns/named.conf.local
 sudo sudo mv ressource/dns/named.conf.local /etc/bind/named.conf.local
 sudo systemctl restart bind9.service
-
-# Faire un check finale de tous les services
-# apt update && apt full-upgrade && apt autoremove --purge && apt autoclean 
-# restart tous les services a la fin uniquement
 
 echo "Fini "
