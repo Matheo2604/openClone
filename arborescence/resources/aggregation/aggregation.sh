cee34e8a96ce97af0ebe0f34e1f3fd17103803fe
Matheo2604 <libeau.matheo@gmail.com>
2024-05-28 16:35:53 +0200

    d

--- arborescence/resources/aggregation/aggregation.sh
+++ arborescence/resources/aggregation/aggregation.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+    apt -y install ifenslave
+    echo ""
+    ip a && ip r
+    echo ""
+    read -p "Entrez le nom de la première interface pour l'agrégation : " interface1
+    read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
+    echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
+    echo -e "\nune nouvelle interface nommer bond0 vient d'etre creer\n" 
+
 echo -e "[aggregation]\n"
 
 sed -i \
