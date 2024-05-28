21dc65ead2df8502ceea47410bb1222d9d208d05
Matheo2604 <libeau.matheo@gmail.com>
2024-05-28 11:23:21 +0200

    s

--- install.sh
+++ install.sh
@@ -291,13 +291,12 @@ systemctl restart apache2.service
 #PB
 mysql  << EOL
 
-  use mariadb 
-  CREATE DATABASE openclone;
-  use openclone;
+  CREATE DATABASE openclonedb;
   CREATE USER 'responsable' IDENTIFIED BY 'felix22';
   grant all privileges on openclone.* to 'responsable';
   CREATE USER 'consultant' IDENTIFIED BY 'felix22';
   GRANT SELECT ON openclone.* TO 'consultant';
+  use openclonedb;
   CREATE TABLE clients(id INT PRIMARY KEY NOT NULL, MAC_Address VARCHAR(17), IP_Address VARCHAR(15),
   Hostname VARCHAR(30));
 
