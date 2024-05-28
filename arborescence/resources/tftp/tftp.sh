e9c0fed610f11930ee47eb7b942a33a7b2bef175
Matheo2604 <libeau.matheo@gmail.com>
2024-05-28 19:57:45 +0200

    d

--- arborescence/resources/tftp/tftp.sh
+++ arborescence/resources/tftp/tftp.sh
@@ -4,9 +4,6 @@ echo -e "[tftp]\n"
 
 apt -y install atftpd
 
-# verify if downloading the paquets atftpd
-mkdir /srv/tftp
-
 cp resources/tftp/atftpd /etc/default/atftpd
 
 chmod -R ugo+rw /srv/tftp/
