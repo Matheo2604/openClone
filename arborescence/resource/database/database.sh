e0dfd761e15c2dee4093cb73417252c0d82b6c16
Matheo2604 <libeau.matheo@gmail.com>
2024-05-28 12:20:50 +0200

    s

--- arborescence/resource/database/database.sh
+++ arborescence/resource/database/database.sh
@@ -4,13 +4,12 @@ apt -y install mariadb-server
 
 mysql  << EOL
 
-  use mariadb 
-  CREATE DATABASE openclone;
-  use openclone;
+  CREATE DATABASE openclonedb;
   CREATE USER 'responsable' IDENTIFIED BY 'felix22';
   grant all privileges on openclone.* to 'responsable';
   CREATE USER 'consultant' IDENTIFIED BY 'felix22';
   GRANT SELECT ON openclone.* TO 'consultant';
+  use openclonedb;
   CREATE TABLE clients(id INT PRIMARY KEY NOT NULL, MAC_Address VARCHAR(17), IP_Address VARCHAR(15),
   Hostname VARCHAR(30));
 
