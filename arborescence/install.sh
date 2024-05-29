7cf45148d7dd4e87330a0db323d7b9675d686682
Matheo2604 <libeau.matheo@gmail.com>
2024-05-29 13:23:09 +0200

    r

--- arborescence/install.sh
+++ arborescence/install.sh
@@ -68,64 +68,63 @@ read -p "Quelle est l'IP du sous résaux LAN (exemple: 192.168.1.0):" IP_LAN_SR
 
 }
 
-    read -p "Voulez-vous mettre en place de l'aggregation de liens ? [y|n] " choice_aggregation
+read -p "Voulez-vous mettre en place de l'aggregation de liens ? [y|n] " choice_aggregation
 
-    if [ "$choice_aggregation" == "y" ]; then
+if [ "$choice_aggregation" == "y" ]; then
 
-        ActivationAggregation=true
-        apt-get -y install ifenslave
-        echo ""
-        ip a && ip r
-        echo ""
-        read -p "Entrez le nom de la première interface pour l'agrégation : " interface1
-        read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
-        echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
-        echo -e "\nune nouvelle interface nommer bond0 vient d'etre creer\n"
+  ActivationAggregation=true
+  apt-get -y install ifenslave
+  echo ""
+  ip a && ip r
+  echo ""
+  read -p "Entrez le nom de la première interface pour l'agrégation : " interface1
+  read -p "Entrez le nom de la deuxième interface pour l'agrégation : " interface2
+  echo "Interfaces sélectionnées pour l'agrégation : $interface1 et $interface2"
+  echo -e "\nune nouvelle interface nommer bond0 vient d'etre creer\n"
 
-    fi
+  fi
 
-    read -p "Voulez-vous mettre en place le système nftables ? [y|n] " choice_nftables
+  read -p "Voulez-vous mettre en place le système nftables ? [y|n] " choice_nftables
 
-    if [ "$choice_nftables" == "y" ]; then
+  if [ "$choice_nftables" == "y" ]; then
 
-        echo "Vous avez choisi d'utiliser nftables."
-        ip a && ip r
-        Recuperer_IP_LAN
-        ActivationNftables=true
+    echo "Vous avez choisi d'utiliser nftables."
+    ip a && ip r
+    Recuperer_IP_LAN
+    ActivationNftables=true
 
-        read -p "Quelle est son interface pour son sous réseaux NAT (exemple: eth0):" Interface_NAT
-        read -p "Quelle sera son addresse IP cote NAT (exemple: 192.168.1.15):" IP_NAT
-        read -p "Quelle est le masque du sous réseaux NAT aux format CIDR (24):" Masque_NAT_CIDR
-        read -p "Quelle est son masque de son sous réseaux NAT (exemple: 255.255.255.0):" Masque_NAT
-        read -p "Quelle est l'IP du sous résaux LAN (exemple: 192.168.1.0):" IP_NAT_SR
-        read -p "Quelle est l'IP du routeur du réseaux NAT (exemple: 192.168.1.254):" Routeur
+    read -p "Quelle est son interface pour son sous réseaux NAT (exemple: eth0):" Interface_NAT
+    read -p "Quelle sera son addresse IP cote NAT (exemple: 192.168.1.15):" IP_NAT
+    read -p "Quelle est le masque du sous réseaux NAT aux format CIDR (24):" Masque_NAT_CIDR
+    read -p "Quelle est son masque de son sous réseaux NAT (exemple: 255.255.255.0):" Masque_NAT
+    read -p "Quelle est l'IP du sous résaux LAN (exemple: 192.168.1.0):" IP_NAT_SR
+    read -p "Quelle est l'IP du routeur du réseaux NAT (exemple: 192.168.1.254):" Routeur
 
-        # Configure Nftables
-        sed -i \
-          -e "s/{Interface_NAT}/$Interface_NAT/g" \
-          -e "s/{IP_NAT_SR}/$IP_NAT_SR/g" \
-          -e "s/{IP_LAN_SR}/$IP_LAN_SR/g" \
-          -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
-          -e "s/{Interface_LAN}/$Interface_LAN/g" \
-          -e "s/{IP_NAT_SR}/$IP_NAT_SR/g" \
-          -e "s/{IP_LAN_SR}/$IP_LAN_SR/g" \
-          -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
-          -e "s/{Masque_NAT_CIDR}/$Masque_NAT_CIDR/g" \
-          resources/nftables/nftables.conf
-        apt-get -y install nftables
-        cp resources/nftables/nftables.conf /etc/nftables.conf
-        systemctl restart nftables
+    # Configure Nftables
+    sed -i \
+      -e "s/{Interface_NAT}/$Interface_NAT/g" \
+      -e "s/{IP_NAT_SR}/$IP_NAT_SR/g" \
+      -e "s/{IP_LAN_SR}/$IP_LAN_SR/g" \
+      -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
+      -e "s/{Interface_LAN}/$Interface_LAN/g" \
+      -e "s/{IP_NAT_SR}/$IP_NAT_SR/g" \
+      -e "s/{IP_LAN_SR}/$IP_LAN_SR/g" \
+      -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
+      -e "s/{Masque_NAT_CIDR}/$Masque_NAT_CIDR/g" \
+      resources/nftables/nftables.conf
+    apt-get -y install nftables
+    cp resources/nftables/nftables.conf /etc/nftables.conf
+    systemctl restart nftables
 
-     else
+  else
 
-        echo ""
-        ip a
-        echo ""
-        Recuperer_IP_LAN
+    echo ""
+    ip a
+    echo ""
+    Recuperer_IP_LA
+    read -p "Quelle est l'IP du routeur du réseaux :" Routeur
 
-        read -p "Quelle est l'IP du routeur du réseaux :" Routeur
-
-    fi
+  fi
 
 case "$ActivationAggregation$ActivationNftables" in
   "truetrue")
@@ -133,14 +132,14 @@ case "$ActivationAggregation$ActivationNftables" in
     #source bash aggregation/aggregation.sh || { echo -e "something went wrong during the installation of the aggregation\nGo see the log on /var/log/openClone" && exit 1; }
     #source bash nftables/nftables.sh || { echo -e "something went wrong during the installation of the nftable\nGo see the log on /var/log/openClone" && exit 1; }
     sed -i \
-    -e "s/{Interface_NAT}/$Interface_NAT/g" \
-    -e "s/{IP_NAT}/$IP_NAT/g" \
-    -e "s/{Masque_NAT_CIDR}/$Masque_NAT_CIDR/g" \
-    -e "s/{Routeur}/$Routeur/g" \
-    -e "s/{IP_LAN}/$IP_LAN/g" \
-    -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
-    -e "s/{interface1}/$interface1/g" \
-    -e "s/{interface2}/$interface2/g" \
+      -e "s/{Interface_NAT}/$Interface_NAT/g" \
+      -e "s/{IP_NAT}/$IP_NAT/g" \
+      -e "s/{Masque_NAT_CIDR}/$Masque_NAT_CIDR/g" \
+      -e "s/{Routeur}/$Routeur/g" \
+      -e "s/{IP_LAN}/$IP_LAN/g" \
+      -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
+      -e "s/{interface1}/$interface1/g" \
+      -e "s/{interface2}/$interface2/g" \
     resources/interface/interfacesAggregationNftables
     cp resources/interface/interfacesAggregationNftables /etc/network/interfaces
     ;;
@@ -149,13 +148,13 @@ case "$ActivationAggregation$ActivationNftables" in
 
     #source bash nftables/nftables.sh || { echo -e "something went wrong during the installation of the nftables\nGo see the log on /var/log/openClone" && exit 1; }
     sed -i \
-    -e "s/{Interface_LAN}/$Interface_LAN/g" \
-    -e "s/{IP_LAN}/$IP_LAN/g" \
-    -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
-    -e "s/{Interface_NAT}/$Interface_NAT/g" \
-    -e "s/{IP_NAT}/$IP_NAT/g" \
-    -e "s/{Masque_NAT_CIDR}/$Masque_NAT_CIDR/g" \
-    -e "s/{Routeur}/$Routeur/g" \
+      -e "s/{Interface_LAN}/$Interface_LAN/g" \
+      -e "s/{IP_LAN}/$IP_LAN/g" \
+      -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
+      -e "s/{Interface_NAT}/$Interface_NAT/g" \
+      -e "s/{IP_NAT}/$IP_NAT/g" \
+      -e "s/{Masque_NAT_CIDR}/$Masque_NAT_CIDR/g" \
+      -e "s/{Routeur}/$Routeur/g" \
     resources/interface/interfacesNftables
     cp resources/interface/interfacesNftables /etc/network/interfaces
     ;;
@@ -164,12 +163,12 @@ case "$ActivationAggregation$ActivationNftables" in
 
     #source bash aggregation/aggregation.sh || { echo -e "something went wrong during the installation of the aggregation\nGo see the log on /var/log/openClone" && exit 1; }
     sed -i \
-    -e "s/{Interface_LAN}/$Interface_LAN/g" \
-    -e "s/{IP_LAN}/$IP_LAN/g" \
-    -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
-    -e "s/{Routeur}/$Routeur/g" \
-    -e "s/{interface1}/$interface1/g" \
-    -e "s/{interface2}/$interface2/g" \
+      -e "s/{Interface_LAN}/$Interface_LAN/g" \
+      -e "s/{IP_LAN}/$IP_LAN/g" \
+      -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
+      -e "s/{Routeur}/$Routeur/g" \
+      -e "s/{interface1}/$interface1/g" \
+      -e "s/{interface2}/$interface2/g" \
     resources/interface/interfacesAggregation
     cp resources/interface/interfacesAggregation /etc/network/interfaces
     ;;
@@ -177,11 +176,11 @@ case "$ActivationAggregation$ActivationNftables" in
   "falsefalse")
 
     sed -i \
-        -e "s/{Interface_LAN}/$Interface_LAN/g" \
-        -e "s/{IP_LAN}/$IP_LAN/g" \
-        -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
-        -e "s/{Routeur}/$Routeur/g" \
-        resources/interface/interfaces
+      -e "s/{Interface_LAN}/$Interface_LAN/g" \
+      -e "s/{IP_LAN}/$IP_LAN/g" \
+      -e "s/{Masque_LAN_CIDR}/$Masque_LAN_CIDR/g" \
+      -e "s/{Routeur}/$Routeur/g" \
+    resources/interface/interfaces
 
     cp resources/interface/interfaces /etc/network/interfaces
     ;;
@@ -201,36 +200,62 @@ apt-get update && apt-get -y upgrade
 # apt-get -y install wget
 #wget https://cdimage.kali.org/kali-2023.4/kali-linux-2023.4-live-amd64.iso
 
+
 echo -e "\nInstallation of the DHCP server\n"
+
 [ $ActivationDHCP ] && log_prefix "dhcp" "resources/dhcp/dhcp.sh" || { echo -e "something went wrong during the installation of the DHCP SERVER\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "DHCP installed & configure correctly\nInstallation of the DNS server\n"
+
 [ $ActivationDNS ] && log_prefix "dns" "resources/dns/dns.sh" || { echo -e "something went wrong during the installation of the DNS SERVER\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "DNS installed & configure correctly\nInstallation of the DataBase server\n"
+
 [ $ActivationMariaDB ] && log_prefix "database" "resources/database/database.sh" || { echo -e "something went wrong during the installation of the DATABASE\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "Database installed & configure correctly\nInstallation of the Web Server\n"
+
 [ $ActivationHTTP ] && log_prefix "http" "resources/http/http.sh" || { echo -e "something went wrong during the installation of the WEB SERVER\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "Web Server installed & configure correctly\nInstallation of the NFS server\n"
+
 [ $ActivationNFS ] && log_prefix "nfs" "resources/nfs/nfs.sh" || { echo -e "something went wrong during the installation of the NFS SERVER\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "NFS installed & configure correctly\nInstallation of the DeBootStrap service\n"
+
 [ $ActivationDeBootStrap ] && log_prefix "debootstrap" "resources/debootstrap/debootstrap.sh" || { echo -e "something went wrong during the installation of the DEBOOTSTRAP\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "DeBootStrap installed & configure correctly\nInstallation of the TFTP server\n"
+
 [ $ActivationTFTP ] && log_prefix "tftp" "resources/tftp/tftp.sh" || { echo -e "something went wrong during the installation of the TFTP SERVER\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "TFTP installed & configure correctly\nCreation of the Boot files for the  maintenace OS\n"
+
 log_prefix "core" "resources/core/core.sh" || { echo -e"something went wrong during the creation of the BOOT FILES for linux\nGo see the log on /var/log/openClone" && exit 1; }
+
 echo -e "Core files create & configure correctly\n"
+
 
 system(){
   {
+
   echo "restarting serices ..."
+
   # Restart every services so they take into account there new configuration
   systemctl restart isc-dhcp-server bind9 atftpd nfs-kernel-server apache2 nftables mariadb
+
   if [ $Kea ]; then
   systemctl restart kea-dhcp4-server
+
   elif [ $Isc ]; then
   systemctl restart isc-dhcp-server
+
   fi
+
   echo "services restart ..."
+
   }2>&1 | sed "s/^/[systemctl] /" >> "$log_file"
+
 }
 
 system || { echo -e "something went wrong during the restart of the services\nGo see the log on /var/log/openClone" && exit 1; }
