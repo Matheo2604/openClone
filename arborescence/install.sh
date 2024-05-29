ed072dffb64698f017ef147171539b4ad76f668e
Matheo2604 <libeau.matheo@gmail.com>
2024-05-29 15:08:21 +0200

    e

--- arborescence/install.sh
+++ arborescence/install.sh
@@ -267,9 +267,9 @@ system(){
 system || { echo -e "something went wrong during the restart of the services\nGo see the log on /var/log/openClone" && exit 1; }
 
 
-[ $Kea ] services=("kea-dhcp4-server" "bind9" "atftpd" "nfs-kernel-server" "apache2" "nftables" "mariadb.service")
+[ $Kea ] && services=("kea-dhcp4-server" "bind9" "atftpd" "nfs-kernel-server" "apache2" "nftables" "mariadb.service")
 
-[ $Isc ] services=("isc-dhcp-server" "bind9" "atftpd" "nfs-kernel-server" "apache2" "nftables" "mariadb.service")
+[ $Isc ] && services=("isc-dhcp-server" "bind9" "atftpd" "nfs-kernel-server" "apache2" "nftables" "mariadb.service")
 
 for service in "${services[@]}"; do
   echo "[$service]"
