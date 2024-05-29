7fe50960b0095f16e59f2c08a23f33de60d0101f
Matheo2604 <libeau.matheo@gmail.com>
2024-05-29 21:25:32 +0200

    j

--- resources/database/database.sh
+++ resources/database/database.sh
@@ -3,16 +3,35 @@
 # Install the package needed for the DataBase
 apt-get -y install mariadb-server
 
-# Create the user & the DataBase
-mysql  << EOL
+# Generat password for admin account 
+if [ $GeneratePasswordMariaDBAdmin ]; then
+  apt -y install pwgen
+  PasswordMariaDBAdmin=$(pwgen -c -n -s 24 1)
+fi
 
-  CREATE DATABASE openclonedb;
-  CREATE USER 'responsable' IDENTIFIED BY 'felix22';
-  grant all privileges on openclone.* to 'responsable';
-  CREATE USER 'consultant' IDENTIFIED BY 'felix22';
-  GRANT SELECT ON openclone.* TO 'consultant';
-  use openclonedb;
-  CREATE TABLE clients(id INT PRIMARY KEY NOT NULL, MAC_Address VARCHAR(17), IP_Address VARCHAR(15),
-  Hostname VARCHAR(30));
+# Change root password fo security
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${PasswordMariaDBAdmin}';"
 
-EOL
+# Create the actual DataBase
+mysql -u root -p"${PasswordMariaDBAdmin}" -e "CREATE DATABASE ${DataBase};"
+
+# Create the user & give it rights
+mysql -u root -p"${PasswordMariaDBAdmin}" -e "CREATE USER '${userMariaDB}'@'localhost' IDENTIFIED BY '${PasswordMariaDBUser}';"
+mysql -u root -p"${PasswordMariaDBAdmin}" -e "GRANT ALL PRIVILEGES ON ${DataBase}.* TO '${userMariaDB}'@'localhost';"
+mysql -u root -p"${PasswordMariaDBAdmin}" -e "FLUSH PRIVILEGES;"
+
+# Create tables
+mysql -u root -p"${PasswordMariaDBAdmin}" -D "${DataBase}" -e "
+CREATE TABLE  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE workstations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    class_id INT,
+    mac_adresse VARCHAR(17),
+    ip_adresse VARCHAR(15),
+    hostname VARCHAR(255),
+    FOREIGN KEY (class_id) REFERENCES class(id)
+);"
