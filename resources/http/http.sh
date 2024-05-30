0a1c4e4e122800271377dbc502daf75fcdc40b84
Matheo2604 <libeau.matheo@gmail.com>
2024-05-30 11:32:35 +0200

    d

--- resources/http/http.sh
+++ resources/http/http.sh
@@ -6,11 +6,11 @@ apt-get -y install apache2 php
 
 # Copied the files for the web interface
 cp resources/http/www $PathHTTP
-cp resources/http/site.conf /etc/apache2/sites-available/
+cp resources/http/opencloneWebSite.conf /etc/apache2/sites-available/
 
 # Start the new site
 a2dissite 000-default.conf
-a2ensite site.conf
+a2ensite opencloneWebSite.conf
 
 # Give the Web Server acces to a folder
 chown www-data:www-data $PathHTTP -Rf
