e7b29b8ea824acfd3feef471d2b4c7b1aebe31eb
Matheo2604 <libeau.matheo@gmail.com>
2024-06-05 21:40:07 +0000

    k

--- install.sh
+++ install.sh
@@ -29,7 +29,6 @@ if [ "$EUID" -ne 0 ];then
 fi
 
 # Verify if the user that start the script is in the openClone folder
-# to test if working when user is in another folder
 cd "$(dirname $0)"
 
 # Get all the variables from config.ini file
