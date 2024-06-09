c309954c30119f165c3fd34aedda545e2360da22
Matheo2604 <libeau.matheo@gmail.com>
2024-06-09 22:58:14 +0000

    h

--- resources/scripts/test.sh
+++ resources/scripts/test.sh
@@ -15,7 +15,8 @@ output=$(./partitionnage.sh "$nombre_partitions")
 
 # Utilisation de read pour extraire les variables de sortie
 read nom_disque taille_partition <<< "$output"
-taille_partition=$((taille_partition - 800000))
+securite=$((nombre_partitions * 2048))
+taille_partition=$((taille_partition - securite))
 
 # Suppression de tout ce qui se trouve sur le disque
 wipefs -a "/dev/$nom_disque"
@@ -25,34 +26,26 @@ dd if=/dev/zero of="/dev/$nom_disque" bs=1M count=10
 parted -s "/dev/$nom_disque" mklabel gpt
 
 # Création d'une partition fat32 pour EFI de 4096000 secteurs
-parted -s "/dev/$nom_disque" mkpart primary fat32 1MiB 2048MiB
+parted -s "/dev/$nom_disque" mkpart primary fat32 1s 4096000s
 parted -s "/dev/$nom_disque" set 1 esp on
 mkfs.fat -F32 "/dev/${nom_disque}1"
 
-# Création d'une partition ext4 pour GRUB de 2048MiB à 4096MiB
-parted -s "/dev/$nom_disque" mkpart primary ext4 2048MiB 4096MiB
+# Création d'une partition ext4 pour GRUB de 4096001s à 8192000s
+parted -s "/dev/$nom_disque" mkpart primary ext4 4096001s 8192000s
 mkfs.ext4 "/dev/${nom_disque}2"
 
-# Calcul de l'offset de départ pour les partitions supplémentaires
-start_sector=$((4096 * 2048))  # 4096MiB in sectors
 
-# Obtenir la taille totale du disque en secteurs
-disk_size=$(parted -m /dev/$nom_disque unit s print | grep "^/dev" | awk -F: '{print $2}' | sed 's/s//')
+# Calcul de l'offset de départ pour les partitions supplémentaires
+start_sector=8192001
 
 # Boucle pour créer les partitions ext4
 for (( i=1; i<=nombre_partitions; i++ ))
 do
   end_sector=$((start_sector + taille_partition - 1))
-  
-  # Vérifier si end_sector dépasse la taille totale du disque
-  if [ $end_sector -ge $disk_size ]; then
-    echo "Erreur : la taille des partitions dépasse la taille du disque."
-    exit 1
-  fi
-  
+  #echo -e "\n\n taille_partition = $taille_partition\n start_sector     = $start_sector\n end_sector       = $end_sector\n\n\n"
   parted -s "/dev/$nom_disque" mkpart primary ext4 "${start_sector}s" "${end_sector}s"
   mkfs.ext4 "/dev/${nom_disque}${i+2}"
-  start_sector=$((end_sector + 1))
+  start_sector=$((end_sector - 2047))
 done
 
 echo "Partitions créées avec succès sur /dev/$nom_disque"
