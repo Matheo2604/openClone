8322e6805d52c259cae7b9c947d0870452710188
Matheo2604 <libeau.matheo@gmail.com>
2024-06-09 22:14:20 +0000

    cd

--- resources/scripts/test.sh
+++ resources/scripts/test.sh
@@ -15,6 +15,11 @@ output=$(./partitionnage.sh "$nombre_partitions")
 nom_disque=$(echo "$output" | awk '{print $1}')
 taille_partition=$(echo "$output" | awk '{print $2}')
 
+#
+#read nom_disque taille_une_partition <<< "$output"
+#taille_une_partition=$((taille_une_partition - 2048))
+#
+
 # Suppression de tout ce qui se trouve sur le disque
 wipefs -a "/dev/$nom_disque"
 dd if=/dev/zero of="/dev/$nom_disque" bs=1M count=10
@@ -28,12 +33,10 @@ parted -s "/dev/$nom_disque" set 1 esp on
 mkfs.fat -F32 "/dev/${nom_disque}1"
 
 # Création d'une partition ext4 pour GRUB de 4096000 secteurs
-start_grub="4096001"
-end_grub="8192000"
-parted -s "/dev/$nom_disque" mkpart primary ext4 ${start_grub}s ${end_grub}s
+parted -s "/dev/$nom_disque" mkpart primary ext4 4096001s 8192000s
 mkfs.ext4 "/dev/${nom_disque}2"
 
-# Création des partitions supplémentaires
+# Création des partitions utilisateur
 start_partition="8192001"
 for i in $(seq 1 $nombre_partitions); do
   end_partition=$((start_partition + taille_partition + 1))
