e91f16f2f18bfccc45b8576e133c9aeab8401fcc
Matheo2604 <libeau.matheo@gmail.com>
2024-06-10 07:44:32 +0000

    test

--- resources/scripts/test.sh
+++ resources/scripts/test.sh
@@ -21,12 +21,12 @@ dd if=/dev/zero of="/dev/$nom_disque" bs=1M count=10
 # Création de la table de partition GPT
 parted -s "/dev/$nom_disque" mklabel gpt
 
-# Création d'une partition fat32 pour EFI de 1 Mo à 1Go+1Mo
+# Création d'une partition fat32 pour EFI de 1 MiO à 1GiO
 parted -s "/dev/$nom_disque" mkpart primary fat32 2048s 2050047s
 parted -s "/dev/$nom_disque" set 1 esp on
 mkfs.fat -F32 "/dev/${nom_disque}1"
 
-# Création d'une partition ext4 pour GRUB de 1Go+1Mo à 2Go+1Mo
+# Création d'une partition ext4 pour GRUB de 1GiO à 2GiO
 parted -s "/dev/$nom_disque" mkpart primary ext4 2050048s 4098047s
 mkfs.ext4 "/dev/${nom_disque}2"
 
@@ -38,7 +38,7 @@ for (( i=1; i<=nombre_partitions; i++ ))
 do
   end_byte=$((start_byte + taille_partition - 1))  # Convertir taille_partition de secteurs en octets
   # Création de la partition avec des unités en octets
-  parted -s "/dev/$nom_disque" mkpart primary ext4 "${start_byte}s" "${end_byte}s"
+  parted -s "/dev/$nom_disque mkpart primary ext4 ${start_byte}s ${end_byte}s"
   mkfs.ext4 "/dev/${nom_disque}${i+2}"
   start_byte=$((end_byte + 1))
 done
