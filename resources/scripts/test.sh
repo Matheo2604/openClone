7fb8ed148eccf22564e801f087680b6d3b6cffc9
Matheo2604 <libeau.matheo@gmail.com>
2024-06-10 00:02:57 +0000

    f

--- resources/scripts/test.sh
+++ resources/scripts/test.sh
@@ -21,24 +21,24 @@ dd if=/dev/zero of="/dev/$nom_disque" bs=1M count=10
 # Création de la table de partition GPT
 parted -s "/dev/$nom_disque" mklabel gpt
 
-# Création d'une partition fat32 pour EFI de 1 MiB à 2048 MiB
-parted -s "/dev/$nom_disque" mkpart primary fat32 2048s 4116479s
+# Création d'une partition fat32 pour EFI de 1 MiB à 2048 MiB (en octets)
+parted -s "/dev/$nom_disque" mkpart primary fat32 1048576s 209715199s
 parted -s "/dev/$nom_disque" set 1 esp on
 mkfs.fat -F32 "/dev/${nom_disque}1"
 
-# Création d'une partition ext4 pour GRUB de 2049 MiB à 4096 MiB
-parted -s "/dev/$nom_disque" mkpart primary ext4 4116480s 8191999s
+# Création d'une partition ext4 pour GRUB de 2049 MiB à 4096 MiB (en octets)
+parted -s "/dev/$nom_disque" mkpart primary ext4 209715200s 419430399s
 mkfs.ext4 "/dev/${nom_disque}2"
 
 # Calcul de l'offset de départ pour les partitions supplémentaires
-start_sector=8192000  # Secteur de départ pour les partitions supplémentaires
+start_byte=419430400  # Octet de départ pour les partitions supplémentaires
 
 # Boucle pour créer les partitions ext4
 for (( i=1; i<=nombre_partitions; i++ ))
 do
-  end_sector=$((start_sector + taille_partition - 1))
-  # Création de la partition avec des unités en secteurs
-  parted -s "/dev/$nom_disque" mkpart primary ext4 "${start_sector}s" "${end_sector}s"
+  end_byte=$((start_byte + taille_partition*512 - 1))  # Convertir taille_partition de secteurs en octets
+  # Création de la partition avec des unités en octets
+  parted -s "/dev/$nom_disque" mkpart primary ext4 "${start_byte}" "${end_byte}"
   mkfs.ext4 "/dev/${nom_disque}${i+2}"
-  start_sector=$((end_sector + 1))
+  start_byte=$((end_byte + 1))
 done
