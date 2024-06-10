25db845f91565d434931b90b29e54810d59344ad
Matheo2604 <libeau.matheo@gmail.com>
2024-06-10 08:52:52 +0000

    f

--- resources/scripts/partitionnage.sh
+++ resources/scripts/partitionnage.sh
@@ -15,18 +15,17 @@ disque=$(lsblk -b -d | grep disk | sort -k 4 -nr | head -n 1)
 disque_nom=$(echo $disque | awk '{print $1}')
 taille_disque=$(echo $disque | awk '{print $4}')
 
-#passage d'octets à secteur
+# Passage d'octets à secteur
 taille_disque=$((taille_disque / 512))
+# Prise en compte de la partition grub et efi
+taille_disque=$((taille_disque - 4098048))
 
 # Calculer la taille du disque en fonction du diviseur
 disque_partitionner=$((taille_disque / nombre_partition))
 
-# Soustraire 4098048 secteur de la taille du disque l'equivalent de 2 GiO 
-disque_taille_finale=$((disque_partitionner - 4098048))
-
-if [ $disque_taille_finale -lt 4098048 ]; then
+if [ $disque_partitionner -lt 4098048 ]; then
     echo "Erreur : La taille finale du disque est inférieure à 4098048 octets."
     exit 1
 fi
 
-echo "$disque_nom $disque_taille_finale"
+echo "$disque_nom $disque_partitionner"
